8b235bbabac796e40ea4fa302a8e0c399371413d
IFLS <c4816@foxmail.com>
2023-04-24 13:42:05 +0800

    feat: change db

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,18 +8,18 @@ import { UsersModule } from "./users/users.module";
 
 const DBConfig: TypeOrmModuleOptions = {
   type: "mysql",
-  host: "localhost",
+  host: "192.168.31.87",
   port: 3306,
   username: "root",
-  password: "root",
+  password: "j;wztmrf0.0",
   database: "db",
   entities: [],
-  synchronize: true, // 自动同步 生产不要使用
+  // synchronize: true, // 自动同步 生产不要使用
+  synchronize: false, // 自动同步 生产不要使用
   retryDelay: 500, // 重试连接数据库间隔
   retryAttempts: 10, // 重试连接次数
   autoLoadEntities: true // 自动加载实体 entities文件
 };
-
 @Module({
   imports: [AuthModule, UsersModule, TypeOrmModule.forRoot(DBConfig)],
   controllers: [AppController],
